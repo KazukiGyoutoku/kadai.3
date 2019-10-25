108f71c7b130db9add307b228cd56ef23d030278
KazukiGyoutoku <56103935+KazukiGyoutoku@users.noreply.github.com>
2019-10-24 22:57:12 -0700

    Update practice4.php

--- practice4.php
+++ practice4.php
@@ -5,7 +5,7 @@ $arr =[8,10,7,9];
 function max_array($arr){
     $max_number = $arr[0];
     foreach($arr as $a){
-        if($max_number > $a){
+        if($max_number < $a){
             $max_number = $a;
         }
     }      
